6ddee08fe1ab4282d307966fa5a78f8d37087874
Michael P. Daugherty <mike@mpdaugherty.com>
2012-04-13 21:41:11 +0800

    Adding some requirements

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 virtualenv --no-site-packages $DIR/bin/env
-pip install -r requirements
+pip install -r requirements.txt
