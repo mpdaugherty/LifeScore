fe3ba0cadee0c68d92088989db34caf73816a12e
Michael P. Daugherty <mike@mpdaugherty.com>
2012-04-20 14:54:44 +0800

    Adding SES Emailing

--- plugins/github.py
+++ plugins/github.py
@@ -1,35 +1,44 @@
 from pygithub3 import Github
-from subprocess import Popen, PIPE
 from config import GITHUB_LOGIN, GITHUB_PASSWORD
 from . import Plugin
 
 class GithubPlugin(Plugin):
     score = 0
+    numForks = 0
+    numFollowers = 0
+    numRepoWatchers = 0
     message = ''
     base_score = 20
     name = 'GitHub'
 
     def calculate(self):
-        # API Login
-        gh = Github(login=GITHUB_LOGIN, password=GITHUB_PASSWORD)
+        try:
+            # API Login
+            gh = Github(login=GITHUB_LOGIN, password=GITHUB_PASSWORD)
 
-        user = gh.users.get()
-        repos = gh.repos.list().all()
+            user = gh.users.get()
+            repos = gh.repos.list().all()
 
-        numFollowers = user.followers
+            self.numFollowers = user.followers
 
-        numForks = 0
-        numRepoWatchers = 0
-        for repo in repos:
-            numForks += repo.forks
-            numRepoWatchers += repo.watchers
+            numForks = 0
+            numRepoWatchers = 0
+            for repo in repos:
+                numForks += repo.forks
+                numRepoWatchers += repo.watchers
 
-        self.score = 3 * numForks + 2 * numFollowers + 1 * numRepoWatchers
+            self.numForks = numForks
+            self.numRepoWatchers = numRepoWatcher
+        except:
+            # We can use the old data from the datastore to still calculate a score
+            self.updated = False
+
+        self.score = 3 * self.numForks + 2 * self.numFollowers + 1 * self.numRepoWatchers
 
         self.message = '''
   Number of forks for all your repos: {}
   Number of watchers for your repos:  {}
   Number of followers:                {}
-'''.format(numForks, numRepoWatchers, numFollowers)
+'''.format(self.numForks, self.numRepoWatchers, self.numFollowers)
 
 
--- sendmail.py
+++ sendmail.py
@@ -1,8 +1,19 @@
 from email.message import Message
 from subprocess import Popen, PIPE
-from config import USER_EMAIL, FROM_EMAIL
+from config import USER_EMAIL, FROM_EMAIL, EMAIL_METHOD, AWS_KEY, AWS_SECRET
+from sesmail.amazon_sender import AmazonSender
 
 def sendmail(subject, body, to=USER_EMAIL, from_addr=FROM_EMAIL):
+    if EMAIL_METHOD is 'SES':
+        sendmail_ses(subject, body, to, from_addr)
+    else:
+        sendmail_sendmail(subject, body, to, from_addr)
+
+def sendmail_ses(subject, body, to=USER_EMAIL, from_addr=FROM_EMAIL):
+    A = AmazonSender(AWS_KEY, AWS_SECRET)
+    A.send_email(from_addr, to, subject, body)
+
+def sendmail_sendmail(subject, body, to=USER_EMAIL, from_addr=FROM_EMAIL):
     m = Message()
     m.add_header('To', to)
     m.add_header('Subject', subject)
